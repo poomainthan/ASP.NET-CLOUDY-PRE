84bf06d76af7d24af283681f7f5bc6ed6e9a2e19
poomainthan <poomainthan003@gmail.com>
2023-05-18 09:45:40 +0530

    Add project files.

---
+++ Controllers/homeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CLOUDY_PRE_IMPLEMENT.Controllers
+{
+    public class homeController : Controller
+    {
+        // GET: home
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+        public ActionResult about()
+        {
+
+            return View("about");
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CLOUDY_PRE_IMPLEMENT.MvcApplication" Language="C#" %>
